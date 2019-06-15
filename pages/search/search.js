2491652c02a055a0ddce1a4c0f6888378438c47a
zhutai <zhutai@outlook.com>
2019-06-16 00:09:39 +0800

    feat: 新增搜索热词功能

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,3 +1,50 @@
-Page({
+//search.js
+const util = require('../../utils/util.js')
+const api = require('../../utils/http.js')
+const app = getApp()
 
-})
+Page({
+  data: {
+    inputShowed: false,
+    inputVal: "",
+    loading: false,
+    searchHotWords: []
+    // 
+  },
+  onLoad() {
+    wx.showLoading({
+      title: "数据加载中",
+      mask: true
+    })
+    api._get('/book/search-hotwords').then(res => {
+      if (res.ok) {
+        let searchHotWords = res.searchHotWords.splice(0, 12)
+        searchHotWords.forEach(item => {
+          item.color = util.randomColor()
+        })
+        this.setData({ searchHotWords, loading: true })
+      }
+    })
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  }
+});
--- utils/util.js
+++ utils/util.js
@@ -18,7 +18,17 @@ const getUrlKey = name => {
   return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.href) || [, ""])[1].replace(/\+/g, '%20')) || null
 }
 
+const randomColor = () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  let rgb = '(' + r + ',' + g + ',' + b + ')';
+  return rgb
+  // return '#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6);
+}
+
 module.exports = {
+  randomColor : randomColor,
   getUrlKey: getUrlKey,
   formatTime: formatTime
 }
